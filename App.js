a9a80a5ea0d0b2c542e93d21c01be5100a4ab90b
Trever O <toveson9522@gmail.com>
2021-12-30 19:37:06 -0700

    added react-native-paper

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import Styles from './styles';
+
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Button } from 'react-native-paper';
 
 export default function App() {
+
+  const [outputText, setOutputText] = useState(
+    'Open up App.js to start working on your app!'
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+
+        <Text> {outputText} </Text>
+
+        <Button icon="camera" mode="contained" title='Change text' style={styles.button} onPress={() => setOutputText('Button pressed!')}>
+          Press Me!
+        </Button>
+
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    // backgroundColor: '#000000',
+    fontWeight: 'bold',
+    fontSize: 45,
+    margin: 3,
+  }
 });
+
+
+// import * as React from 'react';
+// import { AppRegistry } from 'react-native';
+// import { Provider as PaperProvider } from 'react-native-paper';
+// import { name as appName } from './app.json';
+// // import App from './src/App';
+// import { Button } from 'react-native-paper';
+
+// export default function Main() {
+//   return (
+//     <PaperProvider>
+//       <App />
+//     </PaperProvider>
+//   );
+// }
+
+// AppRegistry.registerComponent(appName, () => Main);
---
+++ styles.js
@@ -0,0 +1,16 @@
+export default function Styles() {
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ffffff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    button: {
+      backgroundColor: '#000000',
+      fontWeight: 'bold',
+      fontSize: 45,
+      margin: 3,
+    }
+  });
+}
